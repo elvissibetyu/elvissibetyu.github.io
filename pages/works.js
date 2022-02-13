20879f92b480392923496c90e9c61eaad0470876
Elvis Sibetyu <elvissibetyu@gmail.com>
2022-02-14 00:09:37 +0200

    chore: remove unused imports

--- pages/works.js
+++ pages/works.js
@@ -3,15 +3,6 @@ import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
 
-import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png'
-import thumbWalknote from '../public/images/works/walknote_eyecatch.png'
-import thumbFourPainters from '../public/images/works/the-four-painters_eyecatch.jpg'
-import thumbMenkiki from '../public/images/works/menkiki_eyecatch.png'
-import thumbModeTokyo from '../public/images/works/modetokyo_eyecatch.png'
-import thumbStyly from '../public/images/works/styly_eyecatch.png'
-import thumbPichu2 from '../public/images/works/pichu2_eyecatch.png'
-import thumbFreeDBTagger from '../public/images/works/freedbtagger_eyecatch.png'
-import thumbAmembo from '../public/images/works/amembo_eyecatch.png'
 import mooi from '../public/images/works/mooi.png';
 import commerce from '../public/images/works/commerce.png';
 import intsika from '../public/images/works/intsika.png';
