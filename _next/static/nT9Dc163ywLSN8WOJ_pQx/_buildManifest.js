84bbdde3141cd067b50e1d17d0e444d87dacac99
Elvis Sibetyu <elvissibetyu@gmail.com>
2022-02-14 00:36:12 +0200

    Deploy gh-pages

---
+++ _next/static/nT9Dc163ywLSN8WOJ_pQx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-f7d0263fd8e019a9.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/works":["static/chunks/pages/works-53f6ce30f8924773.js"],sortedPages:["/","/_app","/_error","/works"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
